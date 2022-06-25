707ea0f9dff73ca6cbfd53338e80d090fe5dc93c
sssssssonix <98903063+sssssssonix@users.noreply.github.com>
2022-06-25 12:59:30 +0300

    Update MySuite.scala

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -2,57 +2,63 @@ import scala.collection.immutable.Stream.Empty
 import munit.EmptyPrinter
 
 class MySuite extends munit.FunSuite {
-  test("join test1") {
-    val obtained = Set.join(Set.makeSet(), "", "", "")
-    val expected = ""
-    assertEquals(obtained, expected)
-  }
-
-  test("join test2") {
-    val obtained = Set.join(Set.makeSet(), "[", ", ", "]")
-    val expected = "[]"
+  test("count of element test1") {
+    val obtained = Set.getCountOfElement(Set.makeSet[Int](1,2,5,5,6,5,5,6,9), 5)
+    val expected = 4
     assertEquals(obtained, expected)
   }
   
-  test("join test3") {
-    val obtained = Set.join(Set.makeSet[Int](1), "[", ", ", "]")
-    val expected = "[1]"
+  test("count of element test2") {
+    val obtained = Set.getCountOfElement(Set.makeSet[Int](), 1)
+    val expected = 0
     assertEquals(obtained, expected)
   }
 
-  test("join test4") {
-    val obtained = Set.join(Set.makeSet[Int](1,2), "[", ", ", "]")
-    val expected = "[1, 2]"
+  test("most common element test1") {
+    val obtained = Set.mostCommonElement(Set.makeSet[Int](1,2,5,6,5,5,6,9))
+    val expected = Some(5)
     assertEquals(obtained, expected)
   }
 
-  test("join test5") {
-    val obtained = Set.join(Set.makeSet[Int](1,2,2,3), "[", ", ", "]")
-    val expected = "[1, 2, 3]"
+  test("most common element test2") {
+    val obtained = Set.mostCommonElement(Set.makeSet[Int]())
+    val expected = None
+    assertEquals(obtained, expected)
+  }
+  
+  test("union test1") {
+    val obtained = Set.toString(Set.union(Set.makeSet[Int](1,2,3), Set.makeSet[Int](4,5,6)))
+    val expected = "6x1 5x1 4x1 1x1 2x1 3x1"
     assertEquals(obtained, expected)
   }
 
-  test("xs is subset of ys") {
-    val obtained = Set.isSubsetOf(Set.makeSet[Int](2,3,5), Set.makeSet[Int](1,2,3,4,5,6))
-    val expected = true
+  test("union test2") {
+    val obtained = Set.toString(Set.union(Set.makeSet[Int](1,2,3,3,6), Set.makeSet[Int](4,5,6,2)))
+    val expected = "5x1 4x1 1x1 2x2 6x2 3x2"
     assertEquals(obtained, expected)
   }
 
-  test("xs(empty set) is subset of ys") {
-    val obtained = Set.isSubsetOf(Set.makeSet[Int](), Set.makeSet[Int](1,2,3,4,5,6))
-    val expected = true
+  test("difference test1") {
+    val obtained = Set.toString(Set.difference(Set.makeSet[Int](1,2,5,6,10), Set.makeSet[Int](4,5,6,11)))
+    val expected = "10x1 2x1 1x1"
     assertEquals(obtained, expected)
   }
 
-  test("disjoint") {
-    val obtained = Set.disjoint(Set.makeSet[Int](1,2,3), Set.makeSet[Int](4,5,6))
-    val expected = true
+  test("difference test2") {
+    val obtained = Set.toString(Set.difference(Set.makeSet[Int](5,5,6,5), Set.makeSet[Int]()))
+    val expected = "5x3 6x1"
     assertEquals(obtained, expected)
   }
 
-  test("disjoint test") {
-    val obtained = Set.disjoint(Set.makeSet[Int](1,2,3,4), Set.makeSet[Int](4,5,6))
-    val expected = false
+  test("intersection test1") {
+    val obtained = Set.toString(Set.intersection(Set.makeSet[Int](1,1,6,10,4,6,13), Set.makeSet[Int](1,4,5,10)))
+    val expected = "10x1 4x1 1x1"
+    assertEquals(obtained, expected)
+  }
+
+  test("intersection test2") {
+    val obtained = Set.toString(Set.intersection(Set.makeSet[Int](1,1,6,10,4), Set.makeSet[Int]()))
+    val expected = ""
     assertEquals(obtained, expected)
   }
 }
